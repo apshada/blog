7a0db362d28e7d1026ece82c935a3c730ba6aeb5
Aditya Pratap Singh Hada <apshada1@gmail.com>
2020-10-06 10:49:24 +0530

    changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,9 +20,32 @@ const Content = styled.div`
   padding-top: 0;
 `
 
-const GatsbyLink = styled.a`
-  margin-left: 5px;
+const GitHubLink = styled.a`
+  color: black;
+  margin-left: 15px;
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+
+  ::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    transform-origin: bottom right;
+    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+  }
+
+  :hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
 `
+
 
 const Footer = styled.footer`
   display: flex;
@@ -46,11 +69,10 @@ const Layout = ({ children }) => (
         <Content>
           <main>{children}</main>
           <Footer>
-            <p>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            </p>
-            <GatsbyLink href="https://www.gatsbyjs.org">Gatsby</GatsbyLink>
+            <GitHubLink>Linkedin</GitHubLink>
+            <GitHubLink>Instagram</GitHubLink>
+            <GitHubLink>Mail</GitHubLink>
+            
           </Footer>
         </Content>
       </>
